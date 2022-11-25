29ce3f10f2cdec30f34ff18dd19f0bcf197a661f
timDeHof <this.is.tim.dehof@gmail.com>
2022-11-25 16:24:32 -0500

    removed token user return

--- api/users.js
+++ api/users.js
@@ -1,35 +1,34 @@
-const express = require("express");
-const usersRouter = express.Router();
-const prisma = require("../db/prisma");
-const jwt = require("jsonwebtoken");
-const requireUser = require("./utils");
+const express = require("express")
+const usersRouter = express.Router()
+const prisma = require("../db/prisma")
+const jwt = require("jsonwebtoken")
+const requireUser = require("./utils")
 
 usersRouter.get("/me", requireUser, async (req, res, next) => {
   try {
-    res.send(req.user);
+    res.send(req.user)
   } catch ({ name, message }) {
-    next({ name, message });
+    next({ name, message })
   }
-});
+})
 
 usersRouter.post("/register", async (req, res, next) => {
   try {
-    const { username, password, name, shippingAddress, billingAddress } =
-      req.body;
+    const { username, password, name, shippingAddress, billingAddress } = req.body
     const _user = await prisma.users.findUnique({
       where: {
         username: username,
       },
-    });
+    })
     if (_user) {
-      res.status(401);
+      res.status(401)
       next({
         name: "UserExistsError",
         message: "A user by that username already exists",
-      });
+      })
     } else if (password.length < 8) {
-      res.status(401);
-      next({ name: "PasswordLengthError", message: "Password Too Short!" });
+      res.status(401)
+      next({ name: "PasswordLengthError", message: "Password Too Short!" })
     } else {
       const user = await prisma.users.create({
         data: {
@@ -39,45 +38,43 @@ usersRouter.post("/register", async (req, res, next) => {
           shippingAddress,
           billingAddress,
         },
-      });
+      })
 
       // create a cart
       const cart = await prisma.orders.create({
         data: {
-          userId: user.id
-        }
+          userId: user.id,
+        },
       })
 
       if (!user) {
         next({
           name: "UserCreationError",
           message: "There was a problem registering you. Please try again.",
-        });
+        })
       } else {
         // json token
-        const token = jwt.sign(
-          { id: user.id, username: username },
-          process.env.JWT_SECRET,
-          { expiresIn: "1w" }
-        );
-        res.send({ user, token, message: "you're logged in!" });
+        const token = jwt.sign({ id: user.id, username: username }, process.env.JWT_SECRET, {
+          expiresIn: "1w",
+        })
+        res.send({ user, message: "you're logged in!" })
       }
     }
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 usersRouter.post("/login", async (req, res, next) => {
   // console.log(req, "request");
-  const { username, password } = req.body;
+  const { username, password } = req.body
 
   // request must have both
   if (!username || !password) {
     next({
       name: "MissingCredentialsError",
       message: "Please supply both a username and password",
-    });
+    })
   }
 
   try {
@@ -85,51 +82,46 @@ usersRouter.post("/login", async (req, res, next) => {
       where: {
         username,
       },
-    });
+    })
 
     if (user && user.password === password) {
       // create token & return to user
-      const token = jwt.sign(
-        { id: user.id, username: username },
-        process.env.JWT_SECRET,
-        { expiresIn: "1w" }
-      );
-      res.send({ token, message: "you're logged in!" });
+      const token = jwt.sign({ id: user.id, username: username }, process.env.JWT_SECRET, {
+        expiresIn: "1w",
+      })
+      res.send({ token, message: "you're logged in!" })
     } else {
       next({
         name: "IncorrectCredentialsError",
         message: "Username or password is incorrect",
-      });
+      })
     }
   } catch (error) {
     // console.log(error);
-    next(error);
+    next(error)
   }
-});
-
+})
 
 usersRouter.get("/cart/:userId", async (req, res, next) => {
   try {
     const cart = await prisma.orders.findMany({
       where: {
         userId: +req.params.userId,
-        isActive: true
+        isActive: true,
       },
       include: {
         product_orders: {
           include: {
-            products: true
+            products: true,
           },
-
-        }
-
-      }
-    });
-    res.send(cart);
+        },
+      },
+    })
+    res.send(cart)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 usersRouter.get("/:username", async (req, res, next) => {
   // const username = req.params
@@ -138,20 +130,20 @@ usersRouter.get("/:username", async (req, res, next) => {
       where: {
         username: req.params.username,
       },
-    });
+    })
     //   console.log(getUsername)
-    res.send(getUsername);
+    res.send(getUsername)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 usersRouter.get("/", async (req, res, next) => {
   try {
-    const users = await prisma.users.findMany();
-    res.send(users);
+    const users = await prisma.users.findMany()
+    res.send(users)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
-module.exports = usersRouter;
+})
+module.exports = usersRouter
